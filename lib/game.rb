d378089e618ea0b83d7006fc1ce00bafa60f9da7
David Hale and Qudsia Ahmad <david@>
2017-09-06 13:44:55 -0700

    add script functionality

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,35 @@
+#!/usr/bin/env ruby
+
 class Game
-  def initialize(move)
-    @move = move
+  def initialize(player1)
+
   end
 
-  def beats(p2)
+  def beats()
+    player1  = @player1
 
-    if (p2 === @move)
-     'Tie'
-    elsif (p2 === "scissors" && @move === 'rock')
+    moves = ['rock', 'paper', 'scissors']
+    p2 = moves.sample()
+
+    if (p2 === player1)
+     puts 'It\'s A Tie'
+    elsif (p2 === "scissors" && player1 === 'rock')
       true
-    elsif (p2 === "paper" && @move === 'scissors')
+      puts 'You Win'
+    elsif (p2 === "paper" && player1 === 'scissors')
       true
-    elsif (p2 === "rock" && @move === 'paper')
+      puts 'You Win'
+    elsif (p2 === "rock" && player1 === 'paper')
       true
+      puts 'You Win'
     else
       false
+      puts 'You Lose'
     end
   end
 end
+
+puts "Player1 choose either rock, paper, or scissor."
+player1 = gets.chomp
+newGame = Game.new(player1)
+newGame.beats()
